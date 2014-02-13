0ff556b714bd4476ee98b7c3cd470748b2fd6e5e
D.P. James <dj@deltadev.co.uk>
2014-02-13 23:51:58 +0000

    Moved inline templates out of body of class but still in same file.
    lldb isn't happy in either case.

--- bbi_file.h
+++ bbi_file.h
@@ -50,29 +50,7 @@ public:
   // Obtains data records from r_tree leaf node.
   //
   template <typename T>
-  std::vector<T> records_for_leaf(r_tree::leaf_node ln)
-  {
-    std::vector<T> bdrs;
-    is_.seekg(ln.data_offset);
-
-    uint32_t uncomp_buf_sz = main_hdr.uncompress_buf_size;
-    if (uncomp_buf_sz == 0)
-    {
-      T bdr;
-      while (is_.tellg() < ln.data_offset + ln.data_size)
-      {
-        bdr.unpack(is_);
-        bdrs.push_back(bdr);
-      }
-    }
-    else
-      bdrs = inflate_records<T>(is_, ln.data_size, uncomp_buf_sz);
-
-    
-    return bdrs;
-  }
-
-
+  std::vector<T> records_for_leaf(r_tree::leaf_node ln);
   
 private:
   
@@ -94,73 +72,107 @@ private:
   
 
   // Recursive helper for r_tree search.
+  //
   void recursive_rtree_find(r_tree::node_header nh, data_record r);
   
 
-  // Helper.
+  // Calls zlib to inflate the records.
+  //
   template <typename T>
   std::vector<T>
-  inflate_records(std::istream& is, uint64_t comp_sz, size_t decomp_sz)
-  {
-    int ret;
-    unsigned have;
-    z_stream strm;
-    
-    /* allocate inflate state */
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-    strm.opaque = Z_NULL;
-    strm.avail_in = 0;
-    strm.next_in = Z_NULL;
-    ret = inflateInit(&strm);
-    if (ret != Z_OK) {
-      std::cerr << "couldn't init inflate context.\n";
-      exit(1);
-    }
-    
-    // Buffers.
-    //
-    std::vector<unsigned char> out_buff(decomp_sz);
-    std::vector<unsigned char> in_buff(comp_sz);
-    is.read((char*)in_buff.data(), comp_sz);
-    
-    strm.avail_in = (unsigned)in_buff.size();
-    strm.next_in = in_buff.data();
-    
-    do {
-      strm.avail_out = (unsigned)out_buff.size();
-      strm.next_out = out_buff.data();
-      ret = inflate(&strm, Z_NO_FLUSH);
-      have = (unsigned)out_buff.size() - strm.avail_out;
-      if (strm.avail_in == 0)
-        break;
-      
-    } while (strm.avail_out == 0);
-    
-    if (ret != Z_STREAM_END) {
-      std::cerr << "failed to inflate correctly.\n";
-      exit(1);
-    }
-    inflateEnd(&strm);
-    
-    std::string output(out_buff.begin(), out_buff.end());
-    std::istringstream iss;
-    iss.str(output);
-    
-    std::vector<T> bdrs;
-    while (iss) {
-      T bdr;
-      bdr.unpack(iss);
-      if (bdr.chrom_start != bdr.chrom_end)
-        bdrs.push_back(bdr);
-    }
-    
-    return bdrs;
-  }
-
+  inflate_records(std::istream& is, uint64_t comp_sz, size_t decomp_sz);
+  
 
 };
 
-//#include "bbi_file.hh"
+template <typename T>
+std::vector<T> bbi_file::records_for_leaf(r_tree::leaf_node ln)
+{
+  std::vector<T> bdrs;
+  is_.seekg(ln.data_offset);
+  
+  uint32_t uncomp_buf_sz = main_hdr.uncompress_buf_size;
+  if (uncomp_buf_sz == 0)
+  {
+    T bdr;
+    while (is_.tellg() < ln.data_offset + ln.data_size)
+    {
+      bdr.unpack(is_);
+      bdrs.push_back(bdr);
+    }
+  }
+  else
+  {
+    bdrs = inflate_records<T>(is_, ln.data_size, uncomp_buf_sz);
+  }
+  
+  return bdrs;
+}
+
+template <typename T>
+std::vector<T>
+bbi_file::inflate_records(std::istream& is, uint64_t comp_sz, size_t decomp_sz)
+{
+  int ret;
+  unsigned have;
+  z_stream strm;
+  
+  /* allocate inflate state */
+  strm.zalloc = Z_NULL;
+  strm.zfree = Z_NULL;
+  strm.opaque = Z_NULL;
+  strm.avail_in = 0;
+  strm.next_in = Z_NULL;
+  ret = inflateInit(&strm);
+  if (ret != Z_OK) {
+    std::string err_str = std::string(zError(ret));
+    throw std::runtime_error("bbi_file::inflate_records inflateInit error: " + err_str);
+  }
+  
+  
+  // Buffers.
+  //
+  std::vector<unsigned char> out_buff(decomp_sz);
+  std::vector<unsigned char> in_buff(comp_sz);
+  is.read((char*)in_buff.data(), comp_sz);
+  if (is.gcount() != comp_sz)
+    throw std::runtime_error("bbi_file::inflate_records failed to read comp_sz bytes");
+  
+  strm.avail_in = (unsigned)in_buff.size();
+  strm.next_in = in_buff.data();
+  
+  do {
+    strm.avail_out = (unsigned)out_buff.size();
+    strm.next_out = out_buff.data();
+    ret = inflate(&strm, Z_NO_FLUSH);
+    have = (unsigned)out_buff.size() - strm.avail_out;
+    if (strm.avail_in == 0)
+      break;
+    
+  } while (strm.avail_out == 0);
+  
+  if (ret != Z_STREAM_END) {
+    std::string err_str = "ret != Z_STREAM_END";
+    throw std::runtime_error("bbi_file::inflate_records " + err_str);
+  }
+  
+  
+  inflateEnd(&strm);
+  
+  std::string output(out_buff.begin(), out_buff.end());
+  std::istringstream iss;
+  iss.str(output);
+  
+  std::vector<T> bdrs;
+  while (iss) {
+    T bdr;
+    bdr.unpack(iss);
+    if (bdr.chrom_start != bdr.chrom_end)
+      bdrs.push_back(bdr);
+  }
+  
+  return bdrs;
+}
+
 
 #endif /* _BBI_STREAM_H_ */
