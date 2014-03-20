5e3fc7af58ed0862e969ccd49d12c37c1d17f3ab
D.P. James <dj@deltadev.co.uk>
2014-03-20 00:48:41 +0000

    remote_bbi re-written for streambuf interface.

--- remote_bbi.cc
+++ remote_bbi.cc
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 #include "http_streambuf.h"
 
 #include "bbi_bed_file.h"
 #include "bbi_wig_file.h"
+#include "zoom_record.h"
+#include "wig_record.h"
+#include "bed_record.h"
 
 struct opts
 {
@@ -34,13 +38,25 @@ int main(int argc, char * argv[])
 {
   try {
     
-    
     opts opts = parse_options(argc, argv);
-    dpj::http::streambuf sbuf(opts.host, opts.port, opts.resource, opts.debug_session);
     
-    std::istream is(&sbuf);
+    std::unique_ptr<std::streambuf> sb;
+    if (opts.resource.substr(0, 7) == "http://")
+    {
+      opts.resource = opts.resource.substr(6);
+      sb.reset(new dpj::http::streambuf(opts.host, opts.port, opts.resource, opts.debug_session));
+    }
+    else
+    {
+      auto fb = new std::filebuf();
+      fb->open(opts.resource, std::ios_base::in);
+      if (!fb->is_open())
+        throw std::runtime_error("remote_bbi: could not open file: " + opts.resource);
+      sb.reset(fb);
+    }
+    
+    std::istream is(sb.get());
     bbi::file_base bbi{is};
-    
     
     if (opts.print_main_headers)
     {
@@ -65,72 +81,93 @@ int main(int argc, char * argv[])
     }
     
     auto chrom_id = bbi.chrom_tree.chrom_id(opts.contig);
-    bbi::record r{chrom_id, opts.m, opts.M};
     
     auto index = bbi.index(opts.zoom_level);
-    auto leaves = index.search(r);
+    auto leaves = index.search({chrom_id, opts.m, opts.M});
     
     if (opts.zoom_level > 0)
     {
+      // If we are on a zoom level then everything works the same for either file type.
+      //
       for (auto const& ln : leaves)
       {
-        auto zs = bbi.extract<bbi::zoom::record>(ln);
-        for (auto& z : zs) z.print(std::cout);
-      }
-    }
-    
-    if (bbi.type == bbi::file_type::wig)
-    {
-      is.seekg(0);
-      bbi::wig_file f(is);
-      for (auto const& ln : leaves)
-      {
-        auto wh = f.prepare_records(ln);
-        switch (wh.type)
-        {
-          case static_cast<int>(bbi::wig_file::record_type::bed_graph):
-          {
-            auto ds = f.extract<bbi::wig::bed_graph>(wh.item_count);
-            for (auto const& d : ds) d.print(std::cout);
-
-          }
-            break;
-          case static_cast<int>(bbi::wig_file::record_type::var_step):
-          {
-            auto ds = f.extract<bbi::wig::var_step>(wh.item_count);
-            for (auto const& d : ds) d.print(std::cout);
-          }
-            break;
-          case static_cast<int>(bbi::wig_file::record_type::fixed_step):
-          {
-            auto ds = f.extract<bbi::wig::fixed_step>(wh.item_count);
-            for (auto const& d : ds) d.print(std::cout);
-          }
-            break;
-          default:
-            break;
-        }
-        
-      }
-    }
-    else if (bbi.type == bbi::file_type::bed)
-    {
-      is.seekg(0);
-      bbi::bed_file f(is);
-      for (auto const& ln : leaves)
-      {
-        auto ds = f.extract<bbi::bed::record>(ln);
-        for (auto const& d : ds)
-          d.print(std::cout);
+        std::istream is{bbi.fill_stream(ln)};
+        bbi::zoom::record zr;
+        while(is >> zr)
+          std::cout << zr;
       }
     }
     else
-      throw std::runtime_error("remote_bbi: bad file type.");
+    {
+      // Otherwise we must specialize for wig and bed types.
+      //
+      if (bbi.type == bbi::file_type::wig)
+      {
+        using namespace bbi::wig;
+        using record_type = bbi::wig_file::record_type;
+        bbi::wig_file f(is);
+        
+        for (auto const& ln : leaves)
+        {
+          std::istream record_stream{f.fill_stream(ln)};
+          
+          bbi::wig::header wh;
+          record_stream >> wh;
+          unsigned count = 0;
+          auto type = static_cast<record_type>(wh.type);
+          
+          if (false)
+          {
+          if (type == record_type::bed_graph)
+            for (bed_graph_record bgr; record_stream >> bgr && count < wh.item_count; ++count)
+              std::cout << bgr << '\n';
+          
+          else if (type == record_type::var_step)
+            for (var_step_record vsr; record_stream >> vsr && count < wh.item_count; ++count)
+              std::cout << vsr.chrom_start << ' ' << vsr.val << '\n';
+          
+          else if (type == record_type::fixed_step)
+            for (fixed_step_record fsr; record_stream >> fsr && count < wh.item_count; ++count)
+              std::cout << fsr.val << '\n';
+          
+          else
+            throw std::runtime_error("remote_bbi: bad wig record type.");
+          }
+          else
+            if (type == record_type::bed_graph)
+            {
+              auto rs = extract<bed_graph_record>(record_stream, wh.item_count);
+              std::cerr << "extracted " << rs.size() << " bed_graph_record types.\n";
+            }
+            else if (type == record_type::var_step)
+            {
+              auto rs = extract<var_step_record>(record_stream, wh.item_count);
+              std::cerr << "extracted " << rs.size() << " var step_record types.\n";
+            }
+            else if (type == record_type::fixed_step)
+            {
+              auto rs = extract<fixed_step_record>(record_stream, wh.item_count);
+              std::cerr << "extracted " << rs.size() << " fixed_step_record types.\n";
+            }
+        }
+      }
+      else if (bbi.type == bbi::file_type::bed)
+      {
+        bbi::bed_file f(is);
+        for (auto const& ln : leaves)
+        {
+          std::istream is{f.fill_stream(ln)};
+          bbi::bed::record bdr;
+          while (is >> bdr)
+            std::cout << bdr << '\n';
+        }
+      }
+      else
+        throw std::runtime_error("remote_bbi: bad file type.");
+    }
   }
   catch (std::exception& e)
-  {
-    std::cerr << "Exception: " << e.what() << std::endl;
-  }
+  { std::cerr << "Exception: " << e.what() << std::endl; }
   
   return 0;
   
