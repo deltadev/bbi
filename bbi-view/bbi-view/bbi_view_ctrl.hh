8d947fc0de7173f19245fcd704c57e287153aa95
D.P. James <dj@deltadev.co.uk>
2014-03-21 19:29:26 +0000

    Added c++ side view controller.

---
+++ bbi-view/bbi-view/bbi_view_ctrl.hh
@@ -0,0 +1,17 @@
+#ifndef DPJ_BBI_VIEW_CTRL_HH_
+#define DPJ_BBI_VIEW_CTRL_HH_
+
+class bbi_view_ctrl
+{
+public:
+  bbi_view_ctrl() { }
+
+  template<typename T> filter_stream(bbi::record bbi, std::istream& is)
+  {
+    std::vector<T> rs;
+      return rs;
+  }
+
+};
+
+#endif /* DPJ_BBI_VIEW_CTRL_HH_ */
