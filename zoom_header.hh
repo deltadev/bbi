7a9370dce4f4a541c2d2fb77c277c6bfb0fbae72
D.P. James <dj@deltadev.co.uk>
2014-03-23 17:25:02 +0000

    zoom_record.{h|cc} -> zoom_record.hh

--- zoom_header.hh
+++ zoom_header.hh
@@ -1,9 +1,10 @@
-#ifndef DPJ_ZOOM_HEADER_H_
-#define DPJ_ZOOM_HEADER_H_
+#ifndef DPJ_ZOOM_HEADER_HH_
+#define DPJ_ZOOM_HEADER_HH_
 
 #include <cstdint>
 
-#include <iostream>
+#include <ostream>
+#include <iomanip>
 
 struct zoom_header
 {
@@ -14,11 +15,27 @@ struct zoom_header
   uint64_t data_offset;
   uint64_t index_offset;
 
-  void print(std::ostream& os) const;
-  void pack(std::ostream& os) const;
-  void unpack(std::istream& os);
+  
+  //void pack(std::ostream& os) const;
 
+friend void zoom_print(header const& h, std::ostream& os)
+{
+  using std::setw; using std::left;
+  os << setw(25) << left << "reduction_level" << h.reduction_level << '\n';
+  os << setw(25) << left << "reserved" << h.reserved << '\n';
+  os << setw(25) << left << "data_offset" << h.data_offset << '\n';
+  os << setw(25) << left << "index_offset" << h.index_offset << '\n';
+}
+
+
+  friend void unpack(zoom_header& h, std::streambuf* s)
+  {
+    s->sgetn((char*)&h.reduction_level, sizeof h.reduction_level);
+    s->sgetn((char*)&h.reserved, sizeof h.reserved);
+    s->sgetn((char*)&h.data_offset, sizeof h.data_offset);
+    s->sgetn((char*)&h.index_offset, sizeof h.index_offset);
+  }
 };
 
 
-#endif /* DPJ_ZOOM_HEADER_H_ */
+#endif /* DPJ_ZOOM_HEADER_HH_ */
