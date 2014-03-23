8838fc87a3cb3e412f2e80579b0da796d53ff71e
D.P. James <dj@deltadev.co.uk>
2014-03-23 17:13:37 +0000

    bed_record.(h|cc) -> bed_record.hh

---
+++ bed_record.hh
@@ -0,0 +1,55 @@
+#ifndef DPJ_BED_RECORD_HH_
+#define DPJ_BED_RECORD_HH_
+
+#include "record.h"
+#include <sstream>
+#include <string>
+#include <array>
+/////////////////////////////////////////////////////////////////////
+// bed_record
+//
+/////////////////////////////////////////////////////////////////////
+namespace bbi
+{
+  namespace bed
+  {
+    struct record : bbi::record
+    {
+      std::string rest;
+
+      record() { }
+      record(std::streambuf* s) { unpack(*this, s); }
+      
+      //void pack(std::ostream& os) const;
+
+      friend void print(record const& r, std::ostream& os)
+      {
+        print(static_cast<bbi::record const&>(r), os);
+        os << std::setw(25) << std::left << "rest" << rest << '\n';
+      }  
+
+      friend void unpack(record& r, std::streambuf* s)
+      {        
+        unpack(static_cast<bbi::record&>(r), s);
+        
+        // FIXME: What is a reasonable upper bound for the size of the string rest?
+        //        Have chosen 4 * 1024 here.
+        //
+        std::array<char, 4 * 1024> buff;
+        std::istream is{s};
+        is.getline(buff.data(), 4 * 1024, '\0');
+        std::streamsize count = is.gcount();
+        if (count > 0)
+          r.rest.assign(buff.data(), count - 1);
+      }
+    };        
+  }
+  
+  template <> struct bbi_type<bed::record> : std::true_type { };
+  
+}
+
+
+
+
+#endif /* DPJ_BED_RECORD_HH_ */
