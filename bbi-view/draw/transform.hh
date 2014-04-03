0ac3a6be92dce9f870282883663fa0c6edce55ed
D.P. James <dj@deltadev.co.uk>
2014-04-03 10:45:55 +0100

    new hh files, util and transform, added.

---
+++ bbi-view/draw/transform.hh
@@ -0,0 +1,16 @@
+#ifndef DPJ_TRANSFORM_HH_
+#define DPJ_TRANSFORM_HH_
+
+namespace dpj
+{
+  namespace gl
+  {
+
+    class transform_t
+    {
+  
+    };
+
+  }
+}
+#endif /* DPJ_TRANSFORM_HH_ */
---
+++ bbi-view/draw/util.hh
@@ -0,0 +1,16 @@
+#ifndef DPJ_GL_UTIL_HH_
+#define DPJ_GL_UTIL_HH_
+
+#include <OpenGL/gl3.h>
+
+#include <vector>
+#include <string>
+
+namespace dpj
+{
+  namespace gl
+  {
+
+  }
+}
+#endif /* DPJ_GL_UTIL_HH_ */
