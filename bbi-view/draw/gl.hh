cc32bf05533b738525556dee94f64125027250ed
D.P. James <dj@deltadev.co.uk>
2014-04-05 21:01:13 +0100

    Added gl.hh to include the other files in one hit.

---
+++ bbi-view/draw/gl.hh
@@ -0,0 +1,16 @@
+#ifndef DPJ_GL_HH_
+#define DPJ_GL_HH_
+
+
+#include "shader.hh" // includes <OpenGL/gl3.h>
+#include "program.hh"   // -> shader.hh
+
+#include "transform.hh"
+#include "renderer.hh"  // -> transform.hh 
+#include "drawable.hh"  // -> renderer.hh
+
+
+#include "buffer.hh" // requires <OpenGL/gl3.h>
+
+
+#endif /* DPJ_GL_HH_ */
