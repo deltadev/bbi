6fc91519ae6a86138e1a0b29ddf471ae4fd1a60d
D.P. James <dj@deltadev.co.uk>
2015-03-17 16:14:52 +0000

    Header path updates

--- bbi_stream.hh
+++ bbi_stream.hh
@@ -8,8 +8,8 @@
 
 #include <zlib.h>
 
-#include "block_decompressor.hh"
-#include "http_streambuf.hh"
+#include "dpj/util/block_decompressor.hh"
+#include "dpj/http/http_streambuf.hh"
 
 #include "main_header.hh"
 #include "total_summary_header.hh"
@@ -21,12 +21,12 @@
 
 namespace bbi
 {
-  
+
   class stream : public std::streambuf
   {
   public:
     enum type { wig = 0x888FFC26, bed = 0x8789F2EB } type;
-    
+
     stream(std::string resource)
     {
       if (resource.substr(0, 7) == "http://")
@@ -45,7 +45,7 @@ namespace bbi
           throw std::runtime_error("remote_bbi: could not open file: " + resource);
         input_stream_ = std::move(fb);
       }
-      
+
       // Unpacks main header and determines type.
       //
       unpack(main_header, input_stream_.get());
@@ -61,7 +61,7 @@ namespace bbi
       //
       input_stream()->pubseekpos(main_header.full_data_offset);
       input_stream()->sgetn((char*)&num_records, 8);
-      
+
       // Unpacks the zoom headers.
       //
       input_stream()->pubseekpos(main_header::byte_size);
@@ -72,19 +72,19 @@ namespace bbi
         zoom_headers.push_back(z_h);
       }
     }
-    
+
     std::streambuf* input_stream() { return input_stream_.get(); }
-    
+
     friend stream& seek(stream& s, r_tree::leaf_node ln)
     {
       s.input_buf.resize(ln.data_size);
-      
+
       s.input_stream()->pubseekpos(ln.data_offset);
       std::streamsize n = s.input_stream()->sgetn((char*)s.input_buf.data(), s.input_buf.size());
-      
+
       if (n != ln.data_size)
         throw std::runtime_error("file::inflate_records failed to read comp_sz bytes");
-      
+
       if (s.main_header.uncompress_buf_size == 0)
       {
         s.setg((char*)s.input_buf.data(), (char*)s.input_buf.data(),
@@ -95,10 +95,10 @@ namespace bbi
         auto p = s.decompressor.decompress(s.input_buf.data(), s.input_buf.data() + s.input_buf.size());
         s.setg((char*)p.first, (char*)p.first, (char*)p.second);
       }
-      
+
       return s;
     }
-        
+
     friend index index(stream& s, int level)
     {
       if (level > s.zoom_headers.size())
@@ -106,40 +106,40 @@ namespace bbi
         level = static_cast<int>(s.zoom_headers.size());
         std::cerr << "Warning: zoom level requested was greater than available.\n";
       }
-      
+
       if (level == 0)
         s.input_stream()->pubseekpos(s.main_header.full_index_offset);
       else
         s.input_stream()->pubseekpos(s.zoom_headers[level - 1].index_offset);
-      
+
       return s.input_stream();
     }
-    
+
     friend contig_index
     ctig_index(stream& s)
     {
-      s.input_stream()->pubseekpos(s.main_header.bp_tree_offset);      
+      s.input_stream()->pubseekpos(s.main_header.bp_tree_offset);
       return s.input_stream();
     }
- 
+
     friend void
     print_header(stream& s, std::ostream& os)
     {
       os << "\n**** main_header ****\n\n";
       print(s.main_header, os);
     }
-    
+
     friend void
     print_summary(stream& s, std::ostream& os)
     {
       os << "\n**** total_summary_header ****\n\n";
       print(s.total_summary_header, os);
     }
-    
+
     friend void
     print_num_records(stream& s, std::ostream& os)
     { os << "num records: " << s.num_records << '\n'; }
-    
+
 
     std::vector<zoom_header> zoom_headers;
 
@@ -149,13 +149,13 @@ namespace bbi
     main_header           main_header;
     total_summary_header  total_summary_header;
     std::size_t           num_records;
-    
+
     std::vector<uint8_t> input_buf;
     block_decompressor decompressor;
 
   };
-  
-  
+
+
 }
 
 
